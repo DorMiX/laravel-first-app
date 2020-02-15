14c6c1d01d61793a96afbe12d0bf2ea9df9e7d8d
DorMiX <dormany.robert@gmail.com>
2020-02-15 16:19:23 +0100

    Laracast-END10

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/test', function () {
+//     return view('test', [
+//         'name' => request('name')
+//     ]);
+// });
+
+// Route::get('/posts/{post}', function ($post)
+// {
+//     $posts = [
+//         '1st' => 'First',
+//         '2nd' => 'Second'
+//     ];
+//
+//     if (! array_key_exists($post, $posts)) {
+//         // code...
+//         abort(404, 'Not found!');
+//     }
+//
+//     return view('post', [
+//         'post' => $posts[$post]
+//         // 'post' => $post
+//     ]);
+// });
+
+Route::get('/posts/{post}', 'PostController@show');
