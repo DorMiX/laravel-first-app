0bf98c9e0ceec856bbc75486dc481c61f968b401
DorMiX <dormany.robert@gmail.com>
2020-02-18 15:52:45 +0100

    Laracast-Homework my solution

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,25 @@
+@extends('simplework')
+
+@section('content-about')
+
+    <div id="wrapper">
+        <div class="container">
+            <h2>List of Articles</h2>
+            <span class="byline">by ID</span>
+        </div>
+
+        <div id="wrapper">
+        	<div id="page" class="container">
+        		<ul class="style1">
+                    @foreach ($articles as $article)
+                        <li class="first">
+        					<h3><a href="/articles/{{ $article->id }}"> {{ $article->title }}</a></h3>
+        					<p>{{ $article->excerpt }}</p>
+        				</li>
+                    @endforeach
+        		</ul>
+        	</div>
+        </div>
+    </div>
+
+@endsection
